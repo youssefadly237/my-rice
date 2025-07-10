8d6feed2fd935572e413e28948be41e7649d9712
Jo. Adly <youssefadly237@gmail.com>
2025-07-10 20:09:03 +0300

    fix(treesitter): move ensure_installed and settings under opts block
    - Fixed syntax error due to misplacement of Treesitter config keys
    - Ensures correct plugin behavior at startup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,48 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "c", "python", "lua", "rust" },
+		ensure_installed = {
+			-- Core
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+
+			-- System
+			"c",
+			"cpp",
+			"rust",
+
+			-- Scripting
+			"bash",
+			"python",
+			"fish",
+
+			-- Web
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"json",
+
+			-- Data & config
+			"yaml",
+			"toml",
+			"markdown",
+			"markdown_inline",
+
+			-- Build tools / Git
+			"regex",
+			"make",
+			"cmake",
+			"git_config",
+			"gitcommit",
+			"gitattributes",
+
+			-- Extras
+			"latex",
+		},
+		auto_install = true,
 		highlight = {
 			enable = true,
 		},
