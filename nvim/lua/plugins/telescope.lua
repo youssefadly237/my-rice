797a1e0ff6ba45154490e1a859c5b439ff473ae0
Jo. Adly <youssefadly237@gmail.com>
2025-09-15 13:31:16 +0300

    telescooope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,76 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
   config = function()
     local telescope = require("telescope")
-    telescope.setup()
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        prompt_prefix = " 🔍 ",
+        selection_caret = " ❯ ",
+        path_display = { "truncate" },
+        file_ignore_patterns = {
+          "%.git/",
+          "node_modules/",
+          "%.lock",
+          "__pycache__/",
+          "%.pyc",
+          "target/",
+          "build/",
+          "dist/",
+        },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<Esc>"] = actions.close,
+          },
+        },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = false,
+        },
+        live_grep = {
+          -- Uses default layout
+        },
+        buffers = {
+          previewer = false,
+          initial_mode = "normal",
+        },
+      },
+    })
+
+    -- Load extensions
+    telescope.load_extension("fzf")
 
     -- Keymaps
-    vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
-    vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent Files" })
+    vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Find Commands" })
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+    vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Grep String" })
   end,
 }
